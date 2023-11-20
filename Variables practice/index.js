5454b336d4b82a073411124edcad5bc01540abb0
phgodwin <paige227@hotmail.co.uk>
2023-11-20 12:04:25 +0000

    creating practice repo

---
+++ Variables practice/index.js
@@ -0,0 +1,25 @@
+let myname = "Paige"
+let age = 29
+let surname =null
+let middlename
+let tired = true
+
+console.log (myname)
+console.log (age)
+console.log (surname)
+console.log (middlename)
+console.log (tired)
+
+console.log (typeof myname)
+console.log (typeof age)
+console.log (typeof surname)
+console.log (typeof middlename)
+console.log (typeof tired)
+
+age = "twentynine"
+console.log (age)
+
+const craigage = 39
+console.log (craigage)
+
+craigage = "thirtynine"
