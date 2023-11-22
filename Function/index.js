db960d6c09164d641385e550df59bb9cc4ecf5ec
phgodwin <paige227@hotmail.co.uk>
2023-11-22 16:14:34 +0000

    basic functions practice

---
+++ Function/index.js
@@ -0,0 +1,16 @@
+// Write subjects to subtract, multiply and divide 2 numbers
+
+function divide(num, num1) {
+
+   console.log('Sum:', num/num1)
+}
+
+function multiply(amount, amount1) {
+
+    console.log('Sum:', amount*amount1)
+}
+
+function subtract(digit, digit1) {
+
+    console.log('Sum:', digit-digit1)
+}
