c60dfa9e6d24f31e25dba442ff68906d379fdf8d
phgodwin <paige227@hotmail.co.uk>
2023-11-23 13:59:18 +0000

    function extentions

---
+++ Functions ext/index.js
@@ -0,0 +1,39 @@
+// convert your functions into anonymous functions (they will need variables)
+
+// 1
+
+// function divide(num, num1) {
+
+//     console.log('Sum:', num/num1)
+//  }
+
+// Solution
+// const divide = function (num, num1) {return num/num1}
+
+// 2
+//  function multiply(amount, amount1) {
+ 
+//      console.log('Sum:', amount*amount1)
+//  }
+
+// solution
+// const multiply = function (amount, amount1) { return amount*amount1}
+ 
+// 3
+//  function subtract(digit, digit1) {
+ 
+//      console.log('Sum:', digit-digit1)
+//  }
+
+// solution
+// const subtract = function (digit, digit1) {return digit-digit1}
+
+//  convert them again into arrow functions
+
+const divide = (num,num1)=>num/num1
+
+const multiply = (amount, amount1)=>amount*amount1
+
+const subtract = (digit, digit1)=>digit-digit1
+
+
